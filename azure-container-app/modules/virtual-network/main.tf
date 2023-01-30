a03272771671d8cd47987ac3573b4023bc9f47b9
João Pedro Barboza Garcia <joao_p221@outlook.com>
2023-01-30 17:31:12 -0300

    feat (bastion): removed

--- azure-container-app/modules/virtual-network/main.tf
+++ azure-container-app/modules/virtual-network/main.tf
@@ -36,33 +36,3 @@ resource "azurerm_subnet" "moodle_database" {
   }
 }
 
-resource "azurerm_subnet" "moodle_bastion" {
-  name                 = "AzureBastionSubnet"
-  resource_group_name  = data.azurerm_resource_group.moodle.name
-  virtual_network_name = azurerm_virtual_network.moodle.name
-  address_prefixes     = ["10.0.3.0/27"]
-}
-
-resource "azurerm_public_ip" "moodle_bastion" {
-  name                = "bastion-static-ip"
-  location            = data.azurerm_resource_group.moodle.location
-  resource_group_name = data.azurerm_resource_group.moodle.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-  tags                = var.tags
-}
-
-resource "azurerm_bastion_host" "example" {
-  name                = "${var.azurerm_rg}-bastionhost"
-  location            = data.azurerm_resource_group.moodle.location
-  resource_group_name = data.azurerm_resource_group.moodle.name
-
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.moodle_bastion.id
-    public_ip_address_id = azurerm_public_ip.moodle_bastion.id
-  }
-
-  tags = var.tags
-}
-
